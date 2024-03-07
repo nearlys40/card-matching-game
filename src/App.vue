19ff9ffd39f2619a1721a95d7f42ed2743c1db4f
savisut <savisut@centralpattana.co.th>
2024-03-07 22:59:17 +0700

    Generate deck successfully

--- src/App.vue
+++ src/App.vue
@@ -56,14 +56,30 @@ const restartGame = () => {
   });
 };
 
-for (let i = 0; i < 16; i++) {
+const cartItems = [1, 2, 3, 4, 5, 6, 7, 8];
+
+cartItems.forEach((item) => {
   cardList.value.push({
-    value: i,
+    value: item,
     visible: false,
-    position: i,
+    position: null,
     matched: false,
   });
-}
+
+  cardList.value.push({
+    value: item,
+    visible: false,
+    position: null,
+    matched: false,
+  });
+});
+
+cardList.value = cardList.value.map((card, index) => {
+  return {
+    ...card,
+    position: index,
+  };
+});
 
 const flipCard = (payload) => {
   cardList.value[payload.position].visible = true;
